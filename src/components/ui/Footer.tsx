ff5124c956d7ed47e18d6b73c48094339971159a
Babul Akter <babulakterfsd@gmail.com>
2023-07-23 17:57:06 +0600

    address added on footer

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -2,11 +2,15 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import scrollToElement from 'scroll-to-element';
+import call from '../../../public/assets/images/call.png';
 import fb from '../../../public/assets/images/fby.png';
 import insta from '../../../public/assets/images/instay.png';
 import linkedin from '../../../public/assets/images/linkedin.png';
+import location from '../../../public/assets/images/location.png';
 import logo from '../../../public/assets/images/logo.png';
+import mail from '../../../public/assets/images/mail.png';
 import twitter from '../../../public/assets/images/twittery.png';
+import wtsapp from '../../../public/assets/images/wtsapp.png';
 
 const Footer = () => {
   const router = useRouter();
@@ -20,9 +24,9 @@ const Footer = () => {
       id="footersection"
       className="pt-10 pb-4 lg:pt-16 lg:pb-8 bg-[rgba(18,18,18,1)] text-white grid grid-cols-12 overflow-x-hidden"
     >
-      <div className="col-span-12 lg:col-span-8 lg:col-start-3">
+      <div className="col-span-12 lg:col-span-9 lg:col-start-2">
         <div className="grid grid-cols-12 ">
-          <div className="col-span-12 lg:col-span-4 flex flex-col justify-center items-center gap-y-6 lg:gap-y-0 mb-10 lg:mb-0 lg:mr-auto">
+          <div className="col-span-12 lg:col-span-3 flex flex-col justify-center items-center gap-y-6 lg:gap-y-0 mb-10 lg:mb-0 lg:mr-10">
             <div className="flex justify-center lg:justify-start items-center lg:items-start lg:mb-9">
               <Image
                 src={logo}
@@ -42,9 +46,99 @@ const Footer = () => {
               />
             </div>
           </div>
-          <div className="col-span-12 lg:col-span-8 md:px-8 lg:ml-auto lg:mt-6">
-            <div className="flex justify-center px-3 lg:px-0 items-start gap-x-16 md:gap-x-32">
-              <div className="grid-cols-1 lg:grid-cols-3">
+          <div className="col-span-12 lg:col-span-9 md:px-8  lg:mt-6">
+            <div className="flex justify-center flex-col lg:flex-row px-3 lg:px-0 items-start gap-x-16 md:gap-x-24 gap-y-8">
+              <div className="col-span-12 lg:col-span-3 mx-auto ">
+                <h6
+                  className="text-center mb-2 lg:mb-5 text-xl lg:text-2xl font-[600] font-changa"
+                  style={{
+                    lineHeight: '1.5rem',
+                  }}
+                >
+                  Get In Touch
+                </h6>
+                <div className="flex flex-col gap-y-1.5 md:gap-y-3 text-center text-sm">
+                  <div className="flex items-center gap-x-4">
+                    <Image
+                      src={location}
+                      alt="Location"
+                      width={12}
+                      height={10}
+                      className="hidden object-contain lg:block"
+                    />
+                    <Image
+                      src={location}
+                      alt="Location"
+                      width={16}
+                      height={10}
+                      className="object-contain sm:mt-1 lg:hidden"
+                    />
+                    <p className="font-manrope text-[#868c98] text-[15px]">
+                      3665 As
+                    </p>
+                  </div>
+                  <div className="flex items-center gap-x-4">
+                    <Image
+                      src={call}
+                      alt="Call"
+                      width={15}
+                      height={15}
+                      className="hidden object-contain lg:block"
+                    />
+                    <Image
+                      src={call}
+                      alt="Call"
+                      width={12}
+                      height={10}
+                      className="object-contain lg:hidden"
+                    />
+                    <p className="font-manrope text-[#868c98] text-[15px]">
+                      +32 491 64 21 24
+                    </p>
+                  </div>
+                  <div className="flex items-center gap-x-4">
+                    <Image
+                      src={wtsapp}
+                      alt="Call"
+                      width={15}
+                      height={15}
+                      className="hidden object-contain lg:block"
+                    />
+                    <Image
+                      src={wtsapp}
+                      alt="Call"
+                      width={12}
+                      height={10}
+                      className="object-contain lg:hidden"
+                    />
+                    <p className="font-manrope text-[#868c98] text-[15px]">
+                      +32 491 64 21 24
+                    </p>
+                  </div>
+                  <div className="flex items-center gap-x-4">
+                    <Image
+                      src={mail}
+                      alt="Mail"
+                      width={15}
+                      height={15}
+                      className="hidden object-contain lg:block"
+                    />
+                    <Image
+                      src={mail}
+                      alt="Mail"
+                      width={12}
+                      height={10}
+                      className="object-contain lg:hidden"
+                    />
+                    <p className="font-manrope text-[#868c98] text-[15px]">
+                      <Link href="kieran@kvpainting.be">
+                        kieran@kvpainting.be
+                      </Link>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-span-12 lg:col-span-3 mx-auto">
                 <h6
                   className="text-center mb-2 lg:mb-5 text-xl lg:text-2xl font-[600] font-changa"
                   style={{
@@ -96,7 +190,7 @@ const Footer = () => {
                   </span>
                 </div>
               </div>
-              <div className="grid-cols-1 lg:grid-cols-3">
+              <div className="col-span-12 lg:col-span-3 mx-auto">
                 <h6
                   className="text-center mb-2 md:mb-5 text-xl lg:text-2xl font-[600]  font-manrope tracking-[.32px]"
                   style={{
@@ -126,7 +220,7 @@ const Footer = () => {
                   </span>
                 </div>
               </div>
-              <div className="grid-cols-1 lg:grid-cols-3 hidden lg:block">
+              <div className="col-span-12 lg:col-span-3 hidden lg:block">
                 <h6
                   className="text-center mb-2 md:mb-5 text-xl lg:text-2xl font-[600] font-changa"
                   style={{
